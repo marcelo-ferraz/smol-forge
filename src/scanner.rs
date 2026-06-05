64943e2a8ecdc0aa25cd01899da37034b517cdbf
marcelo <marcelosfm@gmail.com>
2026-06-05 19:27:57 +1200

    keeping string literals as is

--- src/scanner.rs
+++ src/scanner.rs
@@ -28,8 +28,8 @@ pub fn scan_tokens(input: TokenStream) -> syn::Result<TokenStream> {
                         };
 
                         let dsl: TweldDsl = parse2(bracket_group.stream())?;
-
-                        let result = build_string(dsl.tokens);
+                        
+                        let result = build_string(dsl.tokens, &dsl.render_type);
 
                         match dsl.render_type {
                             RenderType::Identifier => {
